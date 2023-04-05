34cc5f1a12c8f5af57cd52e7e95752412b55bd75
Crowdbotics <engineering@crowdbotics.com>
2023-04-05 17:08:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "guui-4",
+  url: "https://guui-4.botics.co",
+  api: "https://guui-4.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
